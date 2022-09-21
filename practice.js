619ecd5807c13e720ae81455db31fd01f47a47fc
Alya_Abdulla <3lya3bdulla@gmail.com>
2022-09-21 15:49:46 +0300

    Avarege ang bigest is Done

--- practice.js
+++ practice.js
@@ -3,8 +3,17 @@ console.log("practice");
 // Write a function called 'average' that accepts a list of
 // numbers and returns the average. The average
 // of an empty list should be considered zero.
-function average() {
-  return undefined;
+function average(num) {
+  let sum=0
+  let avg=0
+  for(let i=0;i<num.length;i++)
+    {
+      sum=sum+num[i]
+      avg=sum/num.length
+    }
+
+  return avg;
+
 }
 console.log(average([1, 2, 3]))
 console.log(average([]))
@@ -12,8 +21,18 @@ console.log(average([]))
 // Write a function called `biggest` that returns the
 // biggest number in an array. If there is no biggest
 // number the function should return undefined.
-function biggest() {
-  return undefined;
+function biggest(num) {
+  let bigestNum=num[0];
+  for(let i=1;i<num.length;i++)
+    {
+      if(bigestNum<num[i])
+      {
+        
+        bigestNum=num[i]
+        
+      }
+    }
+  return bigestNum;
 }
 console.log(biggest([12,3,4,567,8]))
 console.log(biggest([]))
